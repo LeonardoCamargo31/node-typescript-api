fa920d90edee5b9d3ff2489827f6a72a0bc75ca6
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2020-08-12 21:02:14 -0300

    first commit

---
+++ src/util/module-alias.ts
@@ -0,0 +1,14 @@
+import * as path from 'path';
+import moduleAlias from 'module-alias';
+
+// not use more require ('../../../../some/very/deep/module')
+// with module-alias var module = require('@src/module')   
+
+// all files
+const files = path.resolve(__dirname, '../..');
+
+// add alias to files
+moduleAlias.addAliases({
+  '@src': path.join(files, 'src'),
+  '@test': path.join(files, 'test'),
+});
